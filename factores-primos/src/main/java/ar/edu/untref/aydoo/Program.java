33d5bf814f4ae0e3d61b2d917dbb426c655fa1e5
MarcoKlemenc <marcoklemenc@gmail.com>
2017-03-20 23:40:36 -0300

    Se agregó la resolución del ejercicio.

--- factores-primos/src/main/java/ar/edu/untref/aydoo/Program.java
+++ factores-primos/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -1,9 +1,29 @@
 package ar.edu.untref.aydoo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program 
 {
+    public static List<Integer> calcularFactoresPrimos(int numero) {
+		List<Integer> factoresPrimos = new ArrayList<Integer>();
+		while (numero > 1) {
+			for (int i = 2; i <= numero; i++) {
+				if (numero % i == 0) {
+					factoresPrimos.add(i);
+					numero /= i;
+					break;
+				}
+			}
+		}
+		return factoresPrimos;
+	}
     public static final void main(String arg[])
     {
-        System.out.println("hola!");
+        List<Integer> primos = calcularFactoresPrimos(Integer.parseInt(arg[0]));
+		String factores = "Factores primos " + arg[0] + ": ";
+		for (int i = 0; i < primos.size(); i++)
+			factores += primos.get(i) + " ";
+		System.out.println(factores);
     }
 }
