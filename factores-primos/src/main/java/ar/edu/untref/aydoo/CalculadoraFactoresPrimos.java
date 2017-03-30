e398e9c4bb4579b00cba9712e180a36ed2d93ceb
MarcoKlemenc <marcoklemenc@gmail.com>
2017-03-30 16:06:43 -0300

    Arreglo indentación en la calculadora desde GitHub

--- factores-primos/src/main/java/ar/edu/untref/aydoo/CalculadoraFactoresPrimos.java
+++ factores-primos/src/main/java/ar/edu/untref/aydoo/CalculadoraFactoresPrimos.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CalculadoraFactoresPrimos {
-	
-    public static List<Integer> calcularFactoresPrimos(int numero) {
+
+	public static List<Integer> calcularFactoresPrimos(int numero) {
 		List<Integer> factoresPrimos = new ArrayList<Integer>();
 		while (numero > 1) {
 			for (int i = 2; i <= numero; i++) {
@@ -19,12 +19,12 @@ public class CalculadoraFactoresPrimos {
 		return factoresPrimos;
 	}
     
-    public static final void main(String arg[]) {
-        List<Integer> primos = calcularFactoresPrimos(Integer.parseInt(arg[0]));
+	public static final void main(String arg[]) {
+        	List<Integer> primos = calcularFactoresPrimos(Integer.parseInt(arg[0]));
 		String factores = "Factores primos " + arg[0] + ": ";
 		for (int i = 0; i < primos.size(); i++) {
 			factores += primos.get(i) + " ";
 		}
 		System.out.println(factores);
-    }
+	}
 }
