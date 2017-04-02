00b932f10e8c518ec0f5c8882b91cfbfc4fd047b
MarcoKlemenc <marcoklemenc@gmail.com>
2017-04-01 23:04:50 -0300

    Agregado de nuevos tests y uso de aserciones mas exactas.

--- factores-primos-2/src/test/java/ar/edu/untref/aydoo/CalculadoraFactoresPrimosTest.java
+++ factores-primos-2/src/test/java/ar/edu/untref/aydoo/CalculadoraFactoresPrimosTest.java
@@ -41,54 +41,148 @@ public class CalculadoraFactoresPrimosTest {
 	}
 
 	@Test
+	public void testCuarentaEnPretty() {
+		ArrayList<Integer> factores = CalculadoraFactoresPrimos.calcularFactoresPrimos(40);
+		String texto = CalculadoraFactoresPrimos.formatearPretty(factores, "40");
+		String esperado = "Factores primos 40: 2 2 2 5 ";
+		Assert.assertEquals(esperado, texto);
+	}
+
+	@Test
+	public void testCuarentaEnQuiet() {
+		ArrayList<Integer> factores = CalculadoraFactoresPrimos.calcularFactoresPrimos(40);
+		String texto = CalculadoraFactoresPrimos.formatearQuiet(factores);
+		String esperado = "5\n2\n2\n2\n";
+		Assert.assertEquals(esperado, texto);
+	}
+
+	@Test
 	public void testEspecificandoFormatoIncorrecto() {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(baos));
-		String esperado = "Formato no aceptado. Las opciones posibles son: pretty o quiet.";
+		String esperado = "Formato no aceptado. Las opciones posibles son: pretty o quiet.\r\n";
 		String[] argumentos = { "2", "--format=nada" };
 
 		CalculadoraFactoresPrimos.main(argumentos);
 		String salida = new String(baos.toByteArray());
 
-		Assert.assertTrue(salida.contains(esperado));
+		Assert.assertEquals(esperado, salida);
+	}
+
+	@Test
+	public void testUnoSinEspecificarFormato() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		String esperado = "Factores primos 1: \r\n";
+		String[] argumentos = { "1" };
+
+		CalculadoraFactoresPrimos.main(argumentos);
+		String salida = new String(baos.toByteArray());
+
+		Assert.assertEquals(esperado, salida);
+	}
+
+	@Test
+	public void testUnoEspecificandoFormatoPretty() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		String esperado = "Factores primos 1: \r\n";
+		String[] argumentos = { "1", "--format=pretty" };
+
+		CalculadoraFactoresPrimos.main(argumentos);
+		String salida = new String(baos.toByteArray());
+
+		Assert.assertEquals(esperado, salida);
+	}
+
+	@Test
+	public void testUnoEspecificandoFormatoQuiet() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		String esperado = "\r\n";
+		String[] argumentos = { "1", "--format=quiet" };
+
+		CalculadoraFactoresPrimos.main(argumentos);
+		String salida = new String(baos.toByteArray());
+
+		Assert.assertEquals(esperado, salida);
 	}
 
 	@Test
 	public void testDosSinEspecificarFormato() {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(baos));
-		String esperado = "Factores primos 2: 2 ";
+		String esperado = "Factores primos 2: 2 \r\n";
 		String[] argumentos = { "2" };
 
 		CalculadoraFactoresPrimos.main(argumentos);
 		String salida = new String(baos.toByteArray());
 
-		Assert.assertTrue(salida.contains(esperado));
+		Assert.assertEquals(esperado, salida);
 	}
 
 	@Test
 	public void testDosEspecificandoFormatoPretty() {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(baos));
-		String esperado = "Factores primos 2: 2 ";
+		String esperado = "Factores primos 2: 2 \r\n";
 		String[] argumentos = { "2", "--format=pretty" };
 
 		CalculadoraFactoresPrimos.main(argumentos);
 		String salida = new String(baos.toByteArray());
 
-		Assert.assertTrue(salida.contains(esperado));
+		Assert.assertEquals(esperado, salida);
 	}
 
 	@Test
 	public void testDosEspecificandoFormatoQuiet() {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(baos));
-		String esperado = "2";
+		String esperado = "2\n\r\n";
 		String[] argumentos = { "2", "--format=quiet" };
 
 		CalculadoraFactoresPrimos.main(argumentos);
 		String salida = new String(baos.toByteArray());
 
-		Assert.assertTrue(salida.contains(esperado));
+		Assert.assertEquals(esperado, salida);
+	}
+
+	@Test
+	public void testCuarentaSinEspecificarFormato() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		String esperado = "Factores primos 40: 2 2 2 5 \r\n";
+		String[] argumentos = { "40" };
+
+		CalculadoraFactoresPrimos.main(argumentos);
+		String salida = new String(baos.toByteArray());
+
+		Assert.assertEquals(esperado, salida);
+	}
+
+	@Test
+	public void testCuarentaEspecificandoFormatoPretty() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		String esperado = "Factores primos 40: 2 2 2 5 \r\n";
+		String[] argumentos = { "40", "--format=pretty" };
+
+		CalculadoraFactoresPrimos.main(argumentos);
+		String salida = new String(baos.toByteArray());
+
+		Assert.assertEquals(esperado, salida);
+	}
+
+	@Test
+	public void testCuarentaEspecificandoFormatoQuiet() {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		String esperado = "5\n2\n2\n2\n\r\n";
+		String[] argumentos = { "40", "--format=quiet" };
+
+		CalculadoraFactoresPrimos.main(argumentos);
+		String salida = new String(baos.toByteArray());
+
+		Assert.assertEquals(esperado, salida);
 	}
 }
