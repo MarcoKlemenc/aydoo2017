33b8f3c53eb4d56112f318b81e14567c1ec446f9
MarcoKlemenc <marcoklemenc@gmail.com>
2017-04-22 15:41:39 -0300

    Factores primos 3: se copia la solución anterior.
    Se cambia el nombre de las clases y se agregan los xml de estilo y cobertura.

---
+++ factores-primos-3/src/main/java/ar/edu/untref/aydoo/CalculadoraFactoresPrimos3.java
@@ -0,0 +1,66 @@
+package ar.edu.untref.aydoo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+ * Separé el formato del main y la generación de la lista para poder testearlo por separado.
+ * Además no estaba seguro de si iba a poder testear la salida a consola del main.
+ */
+
+public class CalculadoraFactoresPrimos3 {
+
+    public static ArrayList<Integer> calcularFactoresPrimos(int numero) {
+
+        ArrayList<Integer> factoresPrimos = new ArrayList<Integer>();
+        while (numero > 1) {
+            for (int i = 2; i <= numero; i++) {
+                if (numero % i == 0) {
+                    factoresPrimos.add(i);
+                    numero /= i;
+                    break;
+                }
+            }
+        }
+        return factoresPrimos;
+    }
+
+    public static String formatearPretty(ArrayList<Integer> primos, String argumento) {
+
+        String factores = "Factores primos " + argumento + ": ";
+        for (Integer numero : primos) {
+            factores += numero + " ";
+        }
+        return factores;
+    }
+
+    public static String formatearQuiet(ArrayList<Integer> primos) {
+
+        Collections.reverse(primos);
+        String factores = "";
+        for (Integer numero : primos) {
+            factores += numero.toString() + "\n";
+        }
+        return factores;
+    }
+
+    public static final void main(String arg[]) {
+
+        ArrayList<Integer> primos = calcularFactoresPrimos(Integer.parseInt(arg[0]));
+        if (arg.length > 1 && arg[1].substring(0, 9).equals("--format=")) {
+            switch (arg[1].substring(9).toLowerCase()) {
+                case "pretty":
+                    System.out.println(formatearPretty(primos, arg[0]));
+                    break;
+                case "quiet":
+                    System.out.println(formatearQuiet(primos));
+                    break;
+                default:
+                    System.out.println("Formato no aceptado. Las opciones posibles son: pretty o quiet.");
+                    break;
+            }
+        } else {
+            System.out.println(formatearPretty(primos, arg[0]));
+        }
+    }
+}
---
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/CalculadoraFactoresPrimos3Test.java
@@ -0,0 +1,237 @@
+package ar.edu.untref.aydoo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+/*
+ * Uso ByteArrayOutputStream y PrintStream para tomar las salidas por consola que hace el main.
+ * Después las comparo con lo que debería obtener.
+ * Las pruebas giran en base a pocos números porque hay muchos casos cubiertos en las del ejercicio original.
+ * Decidí enfocarme más en probar salidas y lectura de argumentos.
+ */
+public class CalculadoraFactoresPrimos3Test {
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel1EnFormatoPretty() {
+
+        ArrayList<Integer> factores = CalculadoraFactoresPrimos3.calcularFactoresPrimos(1);
+        String esperado = "Factores primos 1: ";
+
+        String texto = CalculadoraFactoresPrimos3.formatearPretty(factores, "1");
+
+        Assert.assertEquals(esperado, texto);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel1EnFormatoQuiet() {
+
+        ArrayList<Integer> factores = CalculadoraFactoresPrimos3.calcularFactoresPrimos(1);
+        String esperado = "";
+
+        String texto = CalculadoraFactoresPrimos3.formatearQuiet(factores);
+
+        Assert.assertEquals(esperado, texto);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel2EnFormatoPretty() {
+
+        ArrayList<Integer> factores = CalculadoraFactoresPrimos3.calcularFactoresPrimos(2);
+        String esperado = "Factores primos 2: 2 ";
+
+        String texto = CalculadoraFactoresPrimos3.formatearPretty(factores, "2");
+
+        Assert.assertEquals(esperado, texto);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel2EnFormatoQuiet() {
+
+        ArrayList<Integer> factores = CalculadoraFactoresPrimos3.calcularFactoresPrimos(2);
+        String esperado = "2\n";
+
+        String texto = CalculadoraFactoresPrimos3.formatearQuiet(factores);
+
+        Assert.assertEquals(esperado, texto);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel40EnFormatoPretty() {
+
+        ArrayList<Integer> factores = CalculadoraFactoresPrimos3.calcularFactoresPrimos(40);
+        String esperado = "Factores primos 40: 2 2 2 5 ";
+
+        String texto = CalculadoraFactoresPrimos3.formatearPretty(factores, "40");
+
+        Assert.assertEquals(esperado, texto);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel40EnFormatoQuiet() {
+
+        ArrayList<Integer> factores = CalculadoraFactoresPrimos3.calcularFactoresPrimos(40);
+        String esperado = "5\n2\n2\n2\n";
+
+        String texto = CalculadoraFactoresPrimos3.formatearQuiet(factores);
+
+        Assert.assertEquals(esperado, texto);
+    }
+
+    @Test
+    public void presentaLosFactoresPrimosEnFormatoPrettyAlEspecificarOtroArgumento() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        String esperado = "Factores primos 2: 2 \n";
+        String[] argumentos = {"2", "--algo=nada"};
+
+        CalculadoraFactoresPrimos3.main(argumentos);
+        String salida = new String(baos.toByteArray());
+
+        Assert.assertEquals(esperado, salida);
+    }
+
+    @Test
+    public void muestraUnErrorAlEspecificarUnFormatoIncorrecto() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        String esperado = "Formato no aceptado. Las opciones posibles son: pretty o quiet.\n";
+        String[] argumentos = {"2", "--format=nada"};
+
+        CalculadoraFactoresPrimos3.main(argumentos);
+        String salida = new String(baos.toByteArray());
+
+        Assert.assertEquals(esperado, salida);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel1EnFormatoPrettyAlIngresarlosPorConsola() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        String esperado = "Factores primos 1: \n";
+        String[] argumentos = {"1", "--format=pretty"};
+
+        CalculadoraFactoresPrimos3.main(argumentos);
+        String salida = new String(baos.toByteArray());
+
+        Assert.assertEquals(esperado, salida);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel1EnFormatoPrettyAlIngresarlosPorConsolaConMayusculas() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        String esperado = "Factores primos 1: \n";
+        String[] argumentos = {"1", "--format=pReTtY"};
+
+        CalculadoraFactoresPrimos3.main(argumentos);
+        String salida = new String(baos.toByteArray());
+
+        Assert.assertEquals(esperado, salida);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel1EnFormatoQuietAlIngresarlosPorConsola() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        String esperado = "\n";
+        String[] argumentos = {"1", "--format=quiet"};
+
+        CalculadoraFactoresPrimos3.main(argumentos);
+        String salida = new String(baos.toByteArray());
+
+        Assert.assertEquals(esperado, salida);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel1EnFormatoQuietAlIngresarlosPorConsolaConMayusculas() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        String esperado = "\n";
+        String[] argumentos = {"1", "--format=qUiEt"};
+
+        CalculadoraFactoresPrimos3.main(argumentos);
+        String salida = new String(baos.toByteArray());
+
+        Assert.assertEquals(esperado, salida);
+    }
+
+    @Test
+    public void presentaLosFactoresPrimosEnFormatoPrettyAlNoEspecificarFormato() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        String esperado = "Factores primos 2: 2 \n";
+        String[] argumentos = {"2"};
+
+        CalculadoraFactoresPrimos3.main(argumentos);
+        String salida = new String(baos.toByteArray());
+
+        Assert.assertEquals(esperado, salida);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel2EnFormatoPrettyAlIngresarlosPorConsola() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        String esperado = "Factores primos 2: 2 \n";
+        String[] argumentos = {"2", "--format=pretty"};
+
+        CalculadoraFactoresPrimos3.main(argumentos);
+        String salida = new String(baos.toByteArray());
+
+        Assert.assertEquals(esperado, salida);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel2EnFormatoQuietAlIngresarlosPorConsola() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        String esperado = "2\n\n";
+        String[] argumentos = {"2", "--format=quiet"};
+
+        CalculadoraFactoresPrimos3.main(argumentos);
+        String salida = new String(baos.toByteArray());
+
+        Assert.assertEquals(esperado, salida);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel40EnFormatoPrettyAlIngresarlosPorConsola() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        String esperado = "Factores primos 40: 2 2 2 5 \n";
+        String[] argumentos = {"40", "--format=pretty"};
+
+        CalculadoraFactoresPrimos3.main(argumentos);
+        String salida = new String(baos.toByteArray());
+
+        Assert.assertEquals(esperado, salida);
+    }
+
+    @Test
+    public void presentaCorrectamenteLosFactoresPrimosDel40EnFormatoQuietAlIngresarlosPorConsola() {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        String esperado = "5\n2\n2\n2\n\n";
+        String[] argumentos = {"40", "--format=quiet"};
+
+        CalculadoraFactoresPrimos3.main(argumentos);
+        String salida = new String(baos.toByteArray());
+
+        Assert.assertEquals(esperado, salida);
+    }
+}
