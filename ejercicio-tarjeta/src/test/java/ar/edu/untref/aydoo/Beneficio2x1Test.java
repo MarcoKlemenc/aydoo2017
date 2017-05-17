34c4c4711088ae262652bc3a7803b5ed7fd3a405
MarcoKlemenc <marcoklemenc@gmail.com>
2017-05-17 16:41:16 -0300

    Tarjeta: se agrega un test de 2x1
    Prueba que no se aplique el beneficio con un solo producto.

--- ejercicio-tarjeta/src/test/java/ar/edu/untref/aydoo/Beneficio2x1Test.java
+++ ejercicio-tarjeta/src/test/java/ar/edu/untref/aydoo/Beneficio2x1Test.java
@@ -23,6 +23,35 @@ public class Beneficio2x1Test {
     }
 
     @Test
+    public void noDebeAplicarConUnSoloProducto() {
+
+        String nombreEstablecimiento = "Nike";
+        Establecimiento nike = new Establecimiento(nombreEstablecimiento);
+
+        Beneficio2x1 beneficio = new Beneficio2x1(nike);
+
+        String nombreZapatillas = "Zapatillas";
+        int precioZapatillas = 90;
+        Producto zapatillas = new Producto(nombreZapatillas, precioZapatillas, nike);
+
+        SortedMap<Producto, Integer> productos = new TreeMap<>();
+        productos.put(zapatillas, 1);
+
+        String nombreCaseros = "Caseros";
+        Sucursal caseros = new Sucursal(nombreCaseros, nike);
+
+        String nombreCliente = "juan";
+        String email = "juan@gmail.com";
+        Cliente juan = new Cliente(nombreCliente, email);
+        TarjetaClassic visa = new TarjetaClassic(juan);
+
+        Compra compra = new Compra(caseros, visa, productos);
+
+        int precioFinal = 90;
+        Assert.assertEquals(precioFinal, beneficio.aplicar(compra));
+    }
+
+    @Test
     public void noDebeAplicarSiPrecioMayorEsMenorA100() {
 
         String nombreEstablecimiento = "Nike";
