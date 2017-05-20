24a1920dbd6ea895cdcc5b1fe751b071be4702bf
MarcoKlemenc <marcoklemenc@gmail.com>
2017-05-20 17:46:56 -0300

    Tarjeta: cambios en tests de descuentos

--- ejercicio-tarjeta/src/test/java/ar/edu/untref/aydoo/DescuentoClassicTest.java
+++ ejercicio-tarjeta/src/test/java/ar/edu/untref/aydoo/DescuentoClassicTest.java
@@ -1,30 +1,66 @@
 package ar.edu.untref.aydoo;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import static org.junit.Assert.assertEquals;
 
 public class DescuentoClassicTest {
 
+    Establecimiento establecimiento;
+
+    @Before
+    public void setUp() {
+
+        establecimiento = new Establecimiento(null);
+    }
+
     @Test(expected = PorcentajeInvalidoException.class)
-    public void debeLanzarExcepcionConPorcentajeInferiorA5()
-            throws PorcentajeInvalidoException {
+    public void debeLanzarExcepcionConPorcentajeInferiorA5() throws PorcentajeInvalidoException {
 
         int porcentaje = 4;
-        DescuentoClassic descuento = new DescuentoClassic(porcentaje, null);
+        Descuento descuento = new DescuentoClassic(porcentaje, null);
     }
 
     @Test
-    public void debeCrearseConExitoConPorcentajeNoInferiorA5()
-            throws PorcentajeInvalidoException {
-
-        String nombre = "Nike";
-        Establecimiento nike = new Establecimiento(nombre);
+    public void debeFijarPorcentaje() throws PorcentajeInvalidoException {
 
         int porcentaje = 10;
-        DescuentoClassic descuento = new DescuentoClassic(porcentaje, nike);
+        Descuento descuento = new DescuentoClassic(porcentaje, establecimiento);
 
         assertEquals(porcentaje, descuento.getPorcentaje());
-        assertEquals(descuento, nike.getBeneficioClassic());
+    }
+
+    @Test
+    public void debeFijarseEnEstablecimiento() throws PorcentajeInvalidoException {
+
+        int porcentaje = 10;
+        Descuento descuento = new DescuentoClassic(porcentaje, establecimiento);
+
+        assertEquals(descuento, establecimiento.getBeneficioClassic());
+    }
+
+    @Test
+    public void debeAplicarseACompra() throws PorcentajeInvalidoException {
+
+        Sucursal sucursal = new Sucursal(null, establecimiento);
+        Cliente cliente = new Cliente(null, null);
+        Tarjeta tarjeta = new TarjetaClassic(cliente);
+        int descuento = 20;
+        Descuento veintePorciento = new DescuentoClassic(descuento, establecimiento);
+
+        String nombreHelado = "Helado";
+        int precio = 10;
+        Producto helado = new Producto(nombreHelado, precio, establecimiento);
+        SortedMap<Producto, Integer> productos = new TreeMap<>();
+        int cantidad = 1;
+        productos.put(helado, cantidad);
+        Compra compra = new Compra(sucursal, tarjeta, productos);
+
+        int total = 8;
+        assertEquals(total, veintePorciento.aplicar(compra));
     }
 }
--- ejercicio-tarjeta/src/test/java/ar/edu/untref/aydoo/DescuentoPremiumTest.java
+++ ejercicio-tarjeta/src/test/java/ar/edu/untref/aydoo/DescuentoPremiumTest.java
@@ -1,14 +1,66 @@
 package ar.edu.untref.aydoo;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import static org.junit.Assert.assertEquals;
 
 public class DescuentoPremiumTest {
 
+    Establecimiento establecimiento;
+
+    @Before
+    public void setUp() {
+
+        establecimiento = new Establecimiento(null);
+    }
+
     @Test(expected = PorcentajeInvalidoException.class)
-    public void debeLanzarExcepcionConPorcentajeInferiorA5()
-            throws PorcentajeInvalidoException {
+    public void debeLanzarExcepcionConPorcentajeInferiorA5() throws PorcentajeInvalidoException {
 
         int porcentaje = 4;
-        DescuentoPremium descuento = new DescuentoPremium(porcentaje, null);
+        Descuento descuento = new DescuentoPremium(porcentaje, null);
+    }
+
+    @Test
+    public void debeFijarPorcentaje() throws PorcentajeInvalidoException {
+
+        int porcentaje = 10;
+        Descuento descuento = new DescuentoPremium(porcentaje, establecimiento);
+
+        assertEquals(porcentaje, descuento.getPorcentaje());
+    }
+
+    @Test
+    public void debeFijarseEnEstablecimiento() throws PorcentajeInvalidoException {
+
+        int porcentaje = 10;
+        Descuento descuento = new DescuentoPremium(porcentaje, establecimiento);
+
+        assertEquals(descuento, establecimiento.getBeneficioPremium());
+    }
+
+    @Test
+    public void debeAplicarseACompra() throws PorcentajeInvalidoException {
+
+        Sucursal sucursal = new Sucursal(null, establecimiento);
+        Cliente cliente = new Cliente(null, null);
+        Tarjeta tarjeta = new TarjetaPremium(cliente);
+        int descuento = 20;
+        Descuento veintePorciento = new DescuentoPremium(descuento, establecimiento);
+
+        String nombreHelado = "Helado";
+        int precio = 10;
+        Producto helado = new Producto(nombreHelado, precio, establecimiento);
+        SortedMap<Producto, Integer> productos = new TreeMap<>();
+        int cantidad = 1;
+        productos.put(helado, cantidad);
+        Compra compra = new Compra(sucursal, tarjeta, productos);
+
+        int total = 8;
+        assertEquals(total, veintePorciento.aplicar(compra));
     }
 }
